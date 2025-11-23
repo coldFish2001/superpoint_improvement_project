42c6814c444bf3a905f6d841d4cf61630b3fd9db
coldFish <coldfish2001@gmail.com>
2025-11-23 13:17:19 +0800

    superpoint-cbam的新分支,没有梯度爆炸的报错

--- settings.py
+++ settings.py
@@ -3,7 +3,8 @@ paths defined here are used in many places
 """
 
 #DATA_PATH = '/home/coldfish/deeplearning/my_files/datasets/' # path for datasets 绝对路径
-DATA_PATH = '../../my_files/datasets/' #相对路径
+#DATA_PATH = '../../my_files/datasets/' #相对路径-main
+DATA_PATH = '/media/coldfish/SN530/BAG数据与双目数据/publicData_coco/' #temp
 
 #EXPER_PATH = '/home/coldfish/deeplearning/my_files/logs/' # path for saving checkpoints 绝对路径
 EXPER_PATH = '../../my_files/logs/' #相对路径
